a3774961e8f84e8ec0227021fea9f35906736a39
Sian Paul Lasaga <sianpaullasaga@gmail.com>
2019-05-08 15:43:35 +0800

    build(App): Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+import Routes from './Routes'
+
+function App() {
+  return (
+    <Fragment>
+      <Routes />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor(props: any) {
+    super(props)
+  }
+
+  public render() {
+    return (
+      <div>
+        <label>Welcome</label>
+      </div>
+    )
+  }
+}
+
+export default Home
