dad7124ed03dc9d745b0d711647e208afed9b68c
pia bonilla <bonillapiabianca@gmail.com>
2019-05-29 15:49:23 +0800

    feat: 🎸 finished profile header

--- src/components/Header/ViewProfile.tsx
+++ src/components/Header/ViewProfile.tsx
@@ -9,21 +9,37 @@ const ViewProfile = () => (
       <span id="header-icon" data-uk-icon="mail" />
       <span id="header-icon" data-uk-icon="plus" />
     </div>
-    <div id="header-tab">
-      <ul data-uk-tab>
-        <li className="uk-active">
-          <a href="#">Profile</a>
-        </li>
-        <li>
-          <a href="#">Store</a>
-        </li>
-        <li>
-          <a href="#">Followers</a>
-        </li>
-        <li>
-          <a href="#">Following</a>
-        </li>
-      </ul>
+    <div id="header">
+      <div id="profile-picture">
+        <img src="https://profilepicturesdp.com/wp-content/uploads/2018/07/profile-avatar-pictures-6.png" />
+      </div>
+      <div id="header-tab">
+        <div id="seller-name">
+          <h3> Anonymous Seller </h3>
+        </div>
+        <ul data-uk-tab>
+          <li className="uk-active">
+            <a href="#" id="tab-label">
+              Profile
+            </a>
+          </li>
+          <li>
+            <a href="#" id="tab-label">
+              Store <span id="label-number"> 28 </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" id="tab-label">
+              Followers <span id="label-number"> 55 </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" id="tab-label">
+              Following <span id="label-number"> 36 </span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 )
