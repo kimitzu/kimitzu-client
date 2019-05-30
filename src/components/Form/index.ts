0431ca2d2c011091f7f143a8bba9b5390cea822e
Sian Paul Lasaga <sianlasaga@kingsland.io>
2019-05-30 18:09:19 +0800

    feat(listing-photos): Add necessary components for listing photos

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,6 +1,7 @@
 import AddressForm from './AddressForm'
 import ListingGeneralForm from './ListingGeneralForm'
+import ListingPhotosForm from './ListingPhotosForm'
 import RegistrationForm from './RegistrationForm'
 import TermsOfService from './TermsOfService'
 
-export { AddressForm, ListingGeneralForm, RegistrationForm, TermsOfService }
+export { AddressForm, ListingGeneralForm, ListingPhotosForm, RegistrationForm, TermsOfService }
---
+++ src/components/Thumbnav/index.ts
@@ -0,0 +1,3 @@
+import ThumbnavSlideshow from './ThumbnavSlideshow'
+
+export { ThumbnavSlideshow }
