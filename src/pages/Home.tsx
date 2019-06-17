76b829963b1e867cd31087f7c76c759c2acd5bdc
Kim Benedict Gulmatico <kim.gulmatico@kingsland.com>
2019-06-17 17:18:52 +0800

    fix(home page): fixing layouts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -155,21 +155,69 @@ class Home extends Component<HomeProps, HomeState> {
           onSearchSubmit={this.handleSearchSubmit}
           isSearchBarShow
         />
-        <div className="uk-flex uk-flex-row uk-flex-left">
-          <div className="uk-width-1-4">
-            <SidebarFilter
-              locationRadius={locationRadius}
-              onChange={this.handleChange}
-              onFilterChange={this.handleFilterChange}
-              onFilterSubmit={this.handleFilterSubmit}
-              plusCode={plusCode}
-              onFilterReset={this.handleFilterReset}
-            />
-          </div>
-          {searchResults.data.length > 0 ? (
-            <div className="uk-flex uk-flex-column uk-width-3-4">
-              <div className="uk-flex uk-flex-right">
-                <div className="uk-width-3-4 uk-margin-top uk-margin-right uk-margin-bottom">
+        <div
+          style={{
+            backgroundColor: 'red',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: 'blue',
+              display: 'flex',
+              flexDirection: 'row',
+              maxWidth: '95%',
+              width: '95%',
+            }}
+          >
+            <div style={{ backgroundColor: 'green', width: '20%' }}>
+              <SidebarFilter
+                locationRadius={locationRadius}
+                onChange={this.handleChange}
+                onFilterChange={this.handleFilterChange}
+                onFilterSubmit={this.handleFilterSubmit}
+                plusCode={plusCode}
+                onFilterReset={this.handleFilterReset}
+              />
+            </div>
+            {searchResults.data.length > 0 ? (
+              <div style={{ backgroundColor: 'violet', width: '80%' }}>
+                <div style={{ backgroundColor: 'pink', display: 'flex', flexDirection: 'column' }}>
+                  <div
+                    style={{
+                      backgroundColor: 'yellow',
+                      display: 'flex',
+                      height: '50px',
+                      alignItems: 'center',
+                      flexDirection: 'row',
+                      justifyContent: 'flex-end',
+                    }}
+                  >
+                    <ul className="uk-pagination">
+                      <li>
+                        <a href="#" onClick={() => this.handlePaginate(paginate.currentPage - 1)}>
+                          <span uk-icon="icon: chevron-left" />
+                        </a>
+                      </li>
+                      {pages.map(p => p)}
+                      <li>
+                        <a href="#" onClick={() => this.handlePaginate(paginate.currentPage + 1)}>
+                          <span uk-icon="icon: chevron-right" />
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="uk-expand">
+                      <FormSelector
+                        options={SortOptions}
+                        defaultVal={this.state.sort}
+                        onChange={event => this.handleSortChange(event.target.value)}
+                      />
+                    </div>
+                  </div>
+                  {/* <div className="uk-flex uk-flex-right">
+                <div className="uk-width-4-4 uk-margin-top uk-margin-right uk-margin-bottom">
                   <div className="uk-flex uk-flex-row uk-flex-right">
                     <div className="uk-margin-small-right">
                       <ul className="uk-pagination">
@@ -195,19 +243,23 @@ class Home extends Component<HomeProps, HomeState> {
                     </div>
                   </div>
                 </div>
+              </div> */}
+                  <div style={{ flex: 1, backgroundColor: 'gray' }}>
+                    <ListingCardGroup data={searchResults.data} />
+                  </div>
+                </div>
               </div>
-              <ListingCardGroup data={searchResults.data} />
-            </div>
-          ) : isSearching ? (
-            <div className="uk-align-center">
-              <div uk-spinner="ratio: 3" />
-            </div>
-          ) : (
-            <div className="uk-align-center">
-              <h2>No Results ¯\_(ツ)_/¯</h2>
-              <p>Try a different search keyword or filter</p>
-            </div>
-          )}
+            ) : isSearching ? (
+              <div className="uk-align-center">
+                <div dat-uk-spinner="ratio: 3" />
+              </div>
+            ) : (
+              <div className="uk-align-center">
+                <h2>No Results ¯\_(ツ)_/¯</h2>
+                <p>Try a different search keyword or filter</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     )
