4e900a58e4bd8cd6cf8f4382f1320a7c4c29c33b
Sian Paul Lasaga <sianlasaga@kingsland.io>
2019-06-19 17:37:52 +0800

    refactor(Registration): Add Djali logo and improve UI

--- src/components/Card/IntroductionCard.tsx
+++ src/components/Card/IntroductionCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const IntroductionCard = (props: Props) => (
   <div id="main-card" className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-    <div id="card-title">
+    <div className="uk-flex uk-flex-center uk-margin-small-top">
       <img id="introduction-logo" src="./images/Logo/Blue/SVG/Djali-Blue-Unique.svg" />
     </div>
     <div id="body-item">
@@ -24,23 +24,6 @@ const IntroductionCard = (props: Props) => (
         Get Started
       </button>
     </div>
-    {/* <div>
-      <div id="body-item">
-        <p id="description-text" className="color-primary">
-          A free market for services
-        </p>
-      </div>
-      <div id="body-item">
-        <a id="buy-sell-text" className="color-primary">
-          New to Djali?
-        </a>
-      </div>
-      <div id="body-item">
-        <button className="uk-button uk-button-primary" onClick={props.handleGetStarted}>
-          Get Started
-        </button>
-      </div>
-    </div> */}
   </div>
 )
 
