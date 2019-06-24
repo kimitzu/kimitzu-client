54a3dd558e9261b823ce73bf01ef97db2a9c672e
24thSaint <rave.arevalo@kingsland.io>
2019-06-24 20:59:33 +0800

    feat(Checkout): Integrate backend API to frontend components

--- src/components/Card/PaymentQRCard.tsx
+++ src/components/Card/PaymentQRCard.tsx
@@ -5,16 +5,26 @@ interface Props {
   qrValue: string
   amount: string // amount + currency
   address: string
+  cryptocurrency: string
   handlePay?: () => void
   handleCopyToClipboard: (field: string) => void
 }
 
-const PaymentQRCard = ({ address, amount, handleCopyToClipboard, handlePay, qrValue }: Props) => (
+const PaymentQRCard = ({
+  address,
+  amount,
+  handleCopyToClipboard,
+  handlePay,
+  cryptocurrency,
+  qrValue,
+}: Props) => (
   <div className="uk-car  d uk-card-default uk-card-body uk-flex uk-flex-row">
-    <QRCode value="test121314asfa asfsfaswqwr faaweweewewfgweg wefwee14124" size={180} />
+    <QRCode value={qrValue} size={180} />
     <div className="uk-padding uk-padding-remove-top uk-padding-remove-bottom">
       <div className="uk-flex uk-flex-middle">
-        <h4 className="uk-text-bold">Pay: {amount}</h4>
+        <h4 className="uk-text-bold">
+          Pay: {amount} {cryptocurrency}
+        </h4>
         <a
           className="text-underline uk-text-small uk-margin-left"
           onClick={() => handleCopyToClipboard('amount')}
@@ -38,12 +48,7 @@ const PaymentQRCard = ({ address, amount, handleCopyToClipboard, handlePay, qrVa
       </div>
       <div>
         <p className="color-secondary">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          Once you have paid, it may take a bit for the interface to update.
         </p>
       </div>
     </div>
