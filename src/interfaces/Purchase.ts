0436a38d23786e76a59aa0344cdd3f89237b3650
24thSaint <rave.arevalo@kingsland.io>
2019-06-24 13:56:02 +0800

    feat(Order, Sale, Purchase): Formalize models

---
+++ src/interfaces/Purchase.ts
@@ -0,0 +1,18 @@
+export interface Purchase {
+  coinType: string
+  moderated: boolean
+  orderId: string
+  paymentCoin: string
+  read: boolean
+  shippingAddress: string
+  shippingName: string
+  slug: string
+  state: string
+  thumbnail: string
+  timestamp: string
+  title: string
+  total: number
+  unreadChatMessages: number
+  vendorHandle: string
+  vendorId: string
+}
---
+++ src/models/Order.test.ts
@@ -0,0 +1,12 @@
+import Order from './Order'
+
+test('Order create test', async () => {
+  // const order = new Order()
+  // const paymentInformation = await order.create(
+  //   'QmehvB3hJJGcA7FBjQfcYFwWbe8jdkf94GW3qoYDCdhCTt',
+  //   1,
+  //   'Hello',
+  //   'TBTC'
+  // )
+  // console.log(paymentInformation)
+})
