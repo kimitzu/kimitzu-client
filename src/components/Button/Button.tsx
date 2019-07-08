ebb8403ea2960d6fbc17905a1f7cb9ff0f793ad5
Sian Paul Lasaga <sianlasaga@kingsland.io>
2019-07-08 14:44:57 +0800

    feat(spinner): Add dot spinner and button with spinner

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import React, { ButtonHTMLAttributes, useEffect, useState } from 'react'
+
+import { DottedSpinner } from '../Spinner'
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  showSpinner?: boolean
+}
+
+const Button = ({ showSpinner, ...props }: Props) => {
+  const [btnWidth, setBtnWidth] = useState(0)
+  const [btnHeight, setBtnHeight] = useState(0)
+  useEffect(() => {
+    const btn = document.getElementById('djali-btn')
+    if ((!btnWidth || !btnHeight) && !showSpinner && btn) {
+      setBtnWidth(btn.offsetWidth)
+      setBtnHeight(btn.offsetHeight)
+    }
+  })
+  return (
+    <button
+      id="djali-btn"
+      style={showSpinner ? { width: `${btnWidth}px`, height: `${btnHeight}px` } : {}}
+      {...props}
+    >
+      {showSpinner ? <DottedSpinner color="#fff" /> : props.children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,3 @@
+import DottedSpinner from './DottedSpinner'
+
+export { DottedSpinner }
