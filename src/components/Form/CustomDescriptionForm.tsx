9ad3bbab70a3e6d89703c4005d430aa584869c59
24thsaint <rave.arevalo@kingsland.io>
2019-12-04 17:14:55 +0800

    feat(settings): implement custom descriptions

--- src/components/Form/CustomDescriptionForm.tsx
+++ src/components/Form/CustomDescriptionForm.tsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { Button } from '../Button'
 import { FormLabel } from '../Label'
 
+import PhysicalCharacteristics from '../../constants/PhysicalCharacteristics.json'
 import { localeInstance } from '../../i18n'
 import { CustomDescription } from '../../interfaces/Profile'
 import Profile from '../../models/Profile'
-import decodeHtml from '../../utils/Unescape'
+
+PhysicalCharacteristics[''] = ''
 
 interface CustomDescriptionProps {
   profile: Profile
@@ -23,8 +25,8 @@ const CustomDescriptionForm = ({ profile }: CustomDescriptionProps) => {
   const [isUpdating, setIsUpdating] = useState(false)
   const [hoverIndex, setHoverIndex] = useState(-1)
 
-  const changeHandler = (label, value, index) => {
-    descriptions[index][label] = value
+  const changeHandler = (field, value, index) => {
+    descriptions[index][field] = value
     setDescriptions([...descriptions])
   }
 
@@ -52,62 +54,91 @@ const CustomDescriptionForm = ({ profile }: CustomDescriptionProps) => {
       }}
     >
       <fieldset className="uk-fieldset">
-        {descriptions.map((description, index) => (
-          <div
-            key={description.label}
-            className="uk-margin uk-flex uk-flex-row uk-flex-middle uk-flex-1"
-            onMouseOver={() => {
-              setHoverIndex(index)
-            }}
-          >
-            <div className="uk-width-1-1">
-              <FormLabel label={customDescriptionForm.labelLabel} />
-              <div className="uk-form-controls">
-                <input
-                  autoFocus
-                  className="uk-input"
-                  type="text"
-                  placeholder={customDescriptionForm.placeholder}
-                  value={descriptions[index].label}
-                  onChange={evt => {
-                    changeHandler('label', evt.target.value, selectedIndex)
-                  }}
-                  onFocus={() => {
-                    setSelectedIndex(index)
-                  }}
-                />
+        {descriptions.map((description, index) => {
+          const descriptionElementValue =
+            PhysicalCharacteristics[descriptions[index] ? descriptions[index].label : '']
+
+          return (
+            <div
+              key={description.label}
+              className="uk-margin uk-flex uk-flex-row uk-flex-middle uk-flex-1"
+              onMouseOver={() => {
+                setHoverIndex(index)
+              }}
+            >
+              <div className="uk-width-1-1">
+                <FormLabel label={customDescriptionForm.labelLabel} />
+                <div className="uk-form-controls">
+                  <select
+                    className="uk-select"
+                    id="form-stacked-select"
+                    onChange={evt => {
+                      changeHandler('label', evt.target.value, selectedIndex)
+                      changeHandler('value', descriptionElementValue[0], selectedIndex)
+                    }}
+                    onFocus={() => {
+                      setSelectedIndex(index)
+                    }}
+                    value={descriptions[index] ? descriptions[index].label : ''}
+                  >
+                    {Object.keys(PhysicalCharacteristics).map((key, pcIndex) => {
+                      return (
+                        <option value={key} key={pcIndex}>
+                          {key}
+                        </option>
+                      )
+                    })}
+                  </select>
+                </div>
+              </div>
+              <div className="uk-margin-left uk-width-1-1">
+                <FormLabel label={customDescriptionForm.valueLabel} />
+                <div className="uk-form-controls">
+                  {Array.isArray(descriptionElementValue) ? (
+                    <select
+                      className="uk-select"
+                      id="form-stacked-select"
+                      onChange={evt => {
+                        changeHandler('value', evt.target.value, selectedIndex)
+                      }}
+                    >
+                      {descriptionElementValue.map((selection, pdIndex) => {
+                        return (
+                          <option key={pdIndex} value={selection}>
+                            {selection}
+                          </option>
+                        )
+                      })}
+                    </select>
+                  ) : (
+                    <input
+                      type={descriptionElementValue}
+                      value={descriptions[index].value}
+                      onChange={evt => {
+                        changeHandler('value', evt.target.value, selectedIndex)
+                      }}
+                      onFocus={() => {
+                        setSelectedIndex(index)
+                      }}
+                      className="uk-input"
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="uk-margin-left uk-width-1-6">
+                {hoverIndex === index ? (
+                  <span
+                    className="uk-margin-top cursor-pointer"
+                    onClick={() => {
+                      deleteHandler(index)
+                    }}
+                    uk-icon="close"
+                  />
+                ) : null}
               </div>
             </div>
-            <div className="uk-margin-left uk-width-1-1">
-              <FormLabel label={customDescriptionForm.valueLabel} />
-              <div className="uk-form-controls">
-                <input
-                  className="uk-input"
-                  type="text"
-                  placeholder={customDescriptionForm.placeholder}
-                  value={decodeHtml(descriptions[index].value)}
-                  onChange={evt => {
-                    changeHandler('value', evt.target.value, selectedIndex)
-                  }}
-                  onFocus={() => {
-                    setSelectedIndex(index)
-                  }}
-                />
-              </div>
-            </div>
-            <div className="uk-margin-left uk-width-1-6">
-              {hoverIndex === index ? (
-                <span
-                  className="uk-margin-top cursor-pointer"
-                  onClick={() => {
-                    deleteHandler(index)
-                  }}
-                  uk-icon="close"
-                />
-              ) : null}
-            </div>
-          </div>
-        ))}
+          )
+        })}
         <div className="uk-margin">
           <Button
             type="button"
