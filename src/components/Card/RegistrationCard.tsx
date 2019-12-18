7bbaf3a630a3a2d3fdb5d01c927bb3539a93ceec
Kingsland <sianpaullasaga@gmail.com>
2019-12-18 20:23:31 +0800

    build(mac): working mac build scripts

--- src/components/Card/RegistrationCard.tsx
+++ src/components/Card/RegistrationCard.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 
+import config from '../../config'
 import { Button } from '../Button'
 
 import './RegistrationCard.css'
@@ -15,7 +16,11 @@ interface Props {
 
 const RegistrationCard = (props: Props) => (
   <div className="uk-card uk-card-default uk-card-body uk-width-1-1 uk-width-1-2@s uk-flex uk-flex-center uk-flex-column uk-flex-middle">
-    <img className="kimitzu-logo-form" src="/images/Logo/full-blue.png" alt="Kimitzu Logo" />
+    <img
+      className="kimitzu-logo-form"
+      src={`${config.host}/images/Logo/full-blue.png`}
+      alt="Kimitzu Logo"
+    />
     <div className="uk-width-1-1 uk-padding-small uk-padding-remove-horizontal">
       {props.content}
     </div>
