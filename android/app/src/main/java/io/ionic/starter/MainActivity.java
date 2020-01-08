99173dbba2ce53418694de6b4700b5a00cb005bc
Sian Paul Lasaga <sianlasaga@kingsland.io>
2020-01-08 16:18:49 +0800

    feat(android ob server): partial implementation for ob server

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,21 +1,57 @@
 package com.kimitzu.dev;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import mobile.Node;
+import mobile.NodeConfig;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
   }
+
+  public void onStart() {
+    super.onStart();
+    startObServer(true);
+  }
+
+  public String createFolder(String name, boolean isTestnet) {
+    String folderPath = Environment.getExternalStorageDirectory().toString() + "/" + name;
+    if (isTestnet) {
+      folderPath += "-testnet";
+    }
+    File folder = new File(folderPath);
+    if (!folder.exists()) {
+      folder.mkdir();
+    }
+    return folderPath;
+  }
+
+  private void startObServer(boolean isTestnet) {
+    NodeConfig nodeConfig = new NodeConfig();
+    String repoPath = createFolder("kimitzu", isTestnet);
+    nodeConfig.setTestnet(isTestnet);
+    nodeConfig.setRepoPath(repoPath);
+    Node node = new Node(nodeConfig, "", "");
+    try {
+      node.start();
+    } catch (Exception e) {
+      Log.e("WTF?", e.toString());
+    }
+  }
+
+
 }
